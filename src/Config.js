b47ac153a4094c2f551b967359e5fa632cae09e4
Santhosh <santhoshkumarstudy@gmail.com>
2023-12-31 18:10:38 +0530

    Launch

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const API = {
     
-    User:'https://santhosh-url-shortner.vercel.app/User',
-    URL:'https://santhosh-url-shortner.vercel.app/Url'    
+    User:'https://santhosh-url-shortner-backend.vercel.app//User',
+    URL:'https://santhosh-url-shortner-backend.vercel.app//Url'    
     
 }
