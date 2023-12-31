059fb932887cd7d020af40480218d291ab60e14d
Santhosh <santhoshkumarstudy@gmail.com>
2023-12-31 17:10:47 +0530

    Launch

---
+++ src/Config.js
@@ -0,0 +1,7 @@
+export const API = {
+    // User:'https://url-short-backend.vercel.app/User',
+    // URL:'https://url-short-backend.vercel.app/Url'
+    User:'http://localhost:3001/User',
+    URL:'http://localhost:3001/Url'
+//      
+}
